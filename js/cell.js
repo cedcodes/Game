44e4fc7139410c5782694687018c48c6e71e447d
Cedrick Garcia <icedgarcia@gmail.com>
2022-01-28 20:11:21 +0800

    Color fix

--- js/cell.js
+++ js/cell.js
@@ -22,7 +22,7 @@ export default class Cell {
   }
   _drawCell() {
     this.ctx.strokeStyle = '#1FEA00';
-    this.ctx.strokeStyle = 'white';
+    this.ctx.strokeStyle = '#1FEA00';
     this.ctx.fillStyle = 'black';
     this.ctx.lineWidth = '2';
     if (this.walls.topWall) this._drawTopWall(this.xCoord, this.yCoord);
